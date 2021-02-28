06bed44b5ed482aa8545aa9b3a742552d06c2ac1
Gabriel Koo <gabriel.koo@bowtie.com.hk>
2021-02-28 17:28:30 +0800

    fix undefined redirectURI field

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 import { Role } from './types';
 
 const getAwsSwitchRoleUrl = (role: Role): string => {
-  const { id, redirectURI: redirect_uri, ...restRoleFields } = role;
+  const { id, redirectURI, ...restRoleFields } = role;
   const cleanedFields = Object.assign(
-    { redirect_uri },
+    { redirect_uri: redirectURI || '' },
     ...Object.entries(restRoleFields)
       .filter(([key, value]) => !(value === undefined || value === null))
       .map(([key, value]) => ({ [key]: value })),
